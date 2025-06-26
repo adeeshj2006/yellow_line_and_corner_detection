380cb8627a222118fa0f983b47671084e4cf0c6a
Adeesh <me24b074@smail.iitm.ac.in>
2025-06-26 14:23:16 +0530

    Small changes to V3

--- log.md
+++ log.md
@@ -1,17 +1,9 @@
 <!-- 
-
     Structure:
 1.  <Commit Message>
     <Usern name>
     - <File Name>
         - <What did you change (make list)>
-
-
-
-
-
-
-
  -->
 
 1.  Added comments
@@ -21,3 +13,9 @@
     - log.md
         - Added log.md
         - Made structure
+
+2.  Small changes to V3
+    adeeshj2006
+    - yellow_corner_V3.py
+        - Added comments and slight modifications to make it more readable
+        - No change in logic
--- yellow_corner_V3.py
+++ yellow_corner_V3.py
@@ -1,3 +1,14 @@
+"""
+
+This file has the 8 segment code
+
+The following are marked on the frame:
+
+Name                Color           Radius          Thickness
+All keypoints:      (100,100,100)   2               2
+
+"""
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -14,14 +25,15 @@ class YellowDepthDetector(Node):
         self.bridge = CvBridge()
         self.depth_image = None
         
-        # 
-        self.threshold = 20  # Minimum size of object (pixels)
-        self.step = 3
+        # Parameters
+        self.threshold = 20     # Minimum size of object (pixels)
+        self.step = 3           # pixel step for 8-segment
         self.cx=0.00
         self.cy=0.00
         
-        self.check = False
+        self.is_centroid_not_yellow = False
         
+        # Subscriptions
         self.rgb_sub = self.create_subscription(Image,'/zed/zed_node/right/image_rect_color',self.rgb_callback,10)
         self.depth_sub = self.create_subscription(Image,'/zed/zed_node/depth/depth_registered',self.depth_callback,10)
         
@@ -30,8 +42,6 @@ class YellowDepthDetector(Node):
         self.orb = cv2.ORB_create(nfeatures=1000)
         
         self.get_logger().info("Yellow L-corner & Depth node initialized.")
-        
-        
 
     def depth_callback(self, msg):
         self.depth_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
@@ -71,29 +81,34 @@ class YellowDepthDetector(Node):
 
                 largest_mask = np.zeros_like(mask)
                 cv2.drawContours(largest_mask, [cnt], -1, 255, thickness=cv2.FILLED)
+
                 # bool for corner detection --> self.check
                 if mask[int(self.cy), int(self.cx)] == 0:
-                    self.check = True
+                    self.is_centroid_not_yellow = True
                 else:
-                    self.check = False
-                cv2.circle(frame,(int(self.cx), int(self.cy)), 8, (255, 0, 0), 3)
-                if (self.check):
+                    self.is_centroid_not_yellow = False
+                cv2.circle(frame,(int(self.cx), int(self.cy)), 8, (255, 0, 0), 3)           # Centroid will be (255,0,0) unless overridden
+
+                if (self.is_centroid_not_yellow):
                     yellow_region = cv2.bitwise_and(frame, frame, mask=largest_mask)
                     gray_yellow = cv2.cvtColor(yellow_region, cv2.COLOR_BGR2GRAY)
+
                     # Apply CLAHE for local contrast enhancement
                     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
                     enhanced = clahe.apply(gray_yellow)
                     blurred = cv2.GaussianBlur(enhanced, (5, 5), 0)
+
                     yellow_kps = self.orb.detect(blurred, mask=largest_mask)  #initially --> keypoints = self.orb.detect(blurred, None)
                     
                     best_corner = None
                     corner_list = []
 
-                    # Here is the one using 8 segments
+                    # Main 8-segment code
                     count = 0
                     for kp in yellow_kps:
                         # Point coords
                         xk, yk = int(kp.pt[0]), int(kp.pt[1])
+                        cv2.circle(frame, (xk, yk), 2, (100,100,100), 2)
 
                         steps = [
                             (xk + self.step, yk),
@@ -107,7 +122,7 @@ class YellowDepthDetector(Node):
                         ]
 
                         for step in steps:
-                            count += mask[int(step[1]), int(step[0])]
+                            count += mask[int(step[1]), int(step[0])]           # Counts the number of segments which are within the mask
 
                         if count <= 4:
                             corner_list.append(kp)
@@ -120,12 +135,12 @@ class YellowDepthDetector(Node):
                         cv2.circle(frame, (int(corner.pt[0]), int(corner.pt[1])), 15, (255, 0, 0), 5)
 
                     if corner_list:
-                        # # best_corner_tuple=min(corner_list, key=lambda x: abs(x[2] - 90.0))
-                        # best_corner=(int(corner_list[0].pt[0]), int(corner_list[0].pt[1])) ## This was the error
-                        # cv2.circle(frame, best_corner, 8, (255, 0, 255), 1)
-                        # corner_z=self.depth_image[best_corner[1],best_corner[0]]
-                        # cv2.putText(frame, f"corner z:{corner_z}", (best_corner[0] + 10, best_corner[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-                        pass
+                        # best_corner_tuple=min(corner_list, key=lambda x: abs(x[2] - 90.0))
+                        best_corner=(int(corner_list[0].pt[0]), int(corner_list[0].pt[1])) ## This was the error
+                        cv2.circle(frame, best_corner, 8, (255, 0, 255), 1)
+                        corner_z=self.depth_image[best_corner[1],best_corner[0]]
+                        cv2.putText(frame, f"corner z:{corner_z}", (best_corner[0] + 10, best_corner[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
+                        # pass
                     # Center point visualisation    
                     cv2.circle(frame,(int(self.cx), int(self.cy)), 8, (255, 0, 255), 3)
                 
